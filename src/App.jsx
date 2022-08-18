776cc443d639130ef11a71e6576fc7856d5b7614
Danylo Prudnikov <danylo.prudnikov@gmail.com>
2022-08-18 16:22:53 +0300

    updated: Header, Search and ToggleUnits components

--- src/App.jsx
+++ src/App.jsx
@@ -2,101 +2,102 @@ import { useState, useEffect } from "react";
 import "./App.scss";
 import Header from "./components/Header";
 import Main from "./components/Main";
-import background from "./assets/background.jpg";
-import 'weather-icons/css/weather-icons.css';
+import "weather-icons/css/weather-icons.css";
 
 const appId = "348d1a1ae88f5ceb7b6797a0de54ffc2";
 const days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
 ];
 
 function App() {
-  const [weatherData, setWeatherData] = useState(null);
-  const [units, setUnits] = useState("metric");
-  const [date, setDate] = useState(null);
-  const [coordinates, setCoordinates] = useState(null);
+	const [weatherData, setWeatherData] = useState(null);
+	const [units, setUnits] = useState("metric");
+	const [date, setDate] = useState(null);
+	const [coordinates, setCoordinates] = useState(null);
 
-  const getCoordinates = () => {
-    const options = {
-      enableHighAccuracy: true,
-      maximumAge: 0,
-    };
+	const getCoordinates = () => {
+		const options = {
+			enableHighAccuracy: true,
+			maximumAge: 0,
+		};
 
-    const error = (error) => console.log(error);
+		const error = (error) => console.log(error);
 
-    navigator.geolocation.getCurrentPosition(
-      (pos) => {
-        setCoordinates(pos.coords);
-      },
-      error,
-      options
-    );
-  };
+		navigator.geolocation.getCurrentPosition(
+			(pos) => {
+				setCoordinates(pos.coords);
+			},
+			error,
+			options
+		);
+	};
 
-  useEffect(() => {
-    getCoordinates();
-  }, []);
+	useEffect(() => {
+		getCoordinates();
+	}, []);
 
-  useEffect(() => {
-    if (coordinates) {
-      fetchWeatherByCoordinates(coordinates.latitude, coordinates.longitude);
-    }
-  }, [coordinates]);
+	useEffect(() => {
+		if (coordinates) {
+			fetchWeatherByCoordinates(
+				coordinates.latitude,
+				coordinates.longitude
+			);
+		}
+	}, [coordinates]);
 
-  const fetchWeather = (city) => {
-    return fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appId}&units=${units}`
-    )
-      .then((response) => response.json())
-      .then((json) => json);
-  };
+	const fetchWeather = (city) => {
+		return fetch(
+			`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appId}&units=${units}`
+		)
+			.then((response) => response.json())
+			.then((json) => json);
+	};
 
-  const fetchWeatherByCoordinates = (lat, lon) => {
-    return fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}&units=${units}`
-    )
-      .then((response) => response.json())
-      .then((json) => setWeatherData(json));
-  };
+	const fetchWeatherByCoordinates = (lat, lon) => {
+		return fetch(
+			`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}&units=${units}`
+		)
+			.then((response) => response.json())
+			.then((json) => setWeatherData(json));
+	};
 
-  const updateWeatherData = async (city) => {
-    setWeatherData(await fetchWeather(city));
-  };
+	const updateWeatherData = async (city) => {
+		setWeatherData(await fetchWeather(city));
+	};
 
-  useEffect(() => {
-    if (weatherData?.name) {
-      updateWeatherData(weatherData?.name);
-      console.log(weatherData);
-    }
-  }, [units]);
+	useEffect(() => {
+		if (weatherData?.name) {
+			updateWeatherData(weatherData?.name);
+			// console.log(weatherData);
+		}
+	}, [units]);
 
-  console.log(weatherData);
+	// console.log(weatherData);
 
-  return (
-    <div className="App" style={{backgroundImage: `url(${background})`}}>
-      <Header
-        setWeatherData={setWeatherData}
-        fetchWeather={fetchWeather}
-        updateWeatherData={updateWeatherData}
-        units={units}
-        setUnits={setUnits}
-      />
-      <Main 
-        weatherData={weatherData} 
-        setDate={setDate}
-        units={units} 
-        date={date} 
-        days={days}
-      />
-    </div>
-    
-  );
+	return (
+		<div className="App">
+			<Header
+				setWeatherData={setWeatherData}
+				fetchWeather={fetchWeather}
+				updateWeatherData={updateWeatherData}
+				units={units}
+				setUnits={setUnits}
+			/>
+			<Main
+				weatherData={weatherData}
+				setDate={setDate}
+				units={units}
+				date={date}
+				days={days}
+			/>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -2,46 +2,46 @@ import { useEffect } from "react";
 import "./style.scss";
 
 const Main = ({ weatherData = null, units, days, setDate, date }) => {
-  useEffect(() => {
-    if (weatherData) {
-      setDate(new Date(weatherData?.dt * 1000));
-    }
-  }, [weatherData]);
+	useEffect(() => {
+		if (weatherData) {
+			setDate(new Date(weatherData?.dt * 1000));
+		}
+	}, [weatherData]);
 
-  return (
-    <main className="main-wrapper">
-      <div>
-        {weatherData?.main ? (
-          <div>
-            <h2 className="city-name">{weatherData?.name}</h2>
-            <h3 className="temperature">
-              {weatherData?.main?.temp.toFixed()}{" "}
-              {units === "metric" ? "C" : "F"}&#176;{" "}
-            </h3>
-            <div className="min-max-temp">
-              <h3 className="min-temp">
-                {weatherData?.main?.temp_min.toFixed()}{" "}
-                {units === "metric" ? "C" : "F"}&#176;{" "}
-              </h3>
-              <h3 className="max-temp">
-                {weatherData?.main?.temp_max.toFixed()}{" "}
-                {units === "metric" ? "C" : "F"}&#176;{" "}
-              </h3>
-            </div>
-          </div>
-        ) : (
-          ""
-        )}
-        <div className="date-container">
-          <p>
-            {weatherData?.main
-              ? `${days[date?.getDay()]} ,${date?.getDate()}`
-              : "The city was not chosen"}
-          </p>
-        </div>
-      </div>
-    </main>
-  );
+	return (
+		<main className="main-wrapper">
+			<div>
+				{weatherData?.main ? (
+					<div>
+						<h2 className="city-name">{weatherData?.name}</h2>
+						<h3 className="temperature">
+							{weatherData?.main?.temp.toFixed()}{" "}
+							{units === "metric" ? "C" : "F"}&#176;{" "}
+						</h3>
+						{/* <div className="min-max-temp">
+							<h3 className="min-temp">
+								{weatherData?.main?.temp_min.toFixed()}{" "}
+								{units === "metric" ? "C" : "F"}&#176;{" "}
+							</h3>
+							<h3 className="max-temp">
+								{weatherData?.main?.temp_max.toFixed()}{" "}
+								{units === "metric" ? "C" : "F"}&#176;{" "}
+							</h3>
+						</div> */}
+					</div>
+				) : (
+					""
+				)}
+				<div className="date-container">
+					<p>
+						{weatherData?.main
+							? `${days[date?.getDay()]}, ${date?.getDate()}`
+							: "The city was not chosen"}
+					</p>
+				</div>
+			</div>
+		</main>
+	);
 };
 
 export default Main;
